1b90056e4beefc99ca1f56990c02bb947f2d9bc6
Shawn M Moore <sartak@bestpractical.com>
2010-12-29 21:25:42 -0500

    SEE ALSO

--- lib/Announcements.pm
+++ lib/Announcements.pm
@@ -8,5 +8,13 @@ __END__
 
 Announcements - communicate across the object network
 
+=head1 SEE ALSO
+
+L<http://www.cincomsmalltalk.com/userblogs/vbykov/blogView?entry=3310034894>
+
+L<http://www.bofh.org.uk/2008/06/29/announcing-announcements-for-ruby>
+
+L<https://github.com/pdcawley/announcements>
+
 =cut
 
