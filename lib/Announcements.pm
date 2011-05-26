4821d71458e48ec838d4aa2566d946b6742e4a8f
Shawn M Moore <sartak@bestpractical.com>
2011-05-26 13:42:10 -0400

    Update the copyright
        Haha how did that happen

--- lib/Announcements.pm
+++ lib/Announcements.pm
@@ -28,7 +28,7 @@ Shawn M Moore, C<sartak@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2010 M Moore.
+Copyright 2010-2011 Shawn M Moore.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
